66c31eb165e7f1c795ad95f98bbf60903677518a
Alan <alanscodelog@gmail.com>
2020-12-04 21:43:24 -0300

    style: minor style adjustment / spelling fixes

--- typedoc.config.js
+++ typedoc.config.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = {
-	readme: 'README.md',
+	readme: "README.md",
 	mode: "modules",
-	inputFiles: [ "src" ],
+	inputFiles: ["src"],
 	out: "docs",
 	excludeNotExported: true,
 	excludePrivate: true,
@@ -13,23 +13,23 @@ module.exports = {
 	ignoreCompilerErrors: true,
 	theme: "./node_modules/typedoc-neo-theme/bin/default",
 	exclude: [
-		"tests/**/*",
 		// not excluding index files causes problems
 		"**/*index.ts",
 	],
+	source: [{
+		path: `${pkg.repository.url}/tree/master/`,
+		line: "L",
+	}],
 	// prevents typedoc autodetecting installed plugins
-	// explitcly listing them makes things easier to debug
+	// explicity listing them makes things easier to debug
 	plugin: [
 		"typedoc-neo-theme",
 		"typedoc-plugin-external-module-name",
 	],
-	"source": [
-		{ "path": pkg.repository + "/tree/master/", "line": "L" }
-	],
-	//topbar
-	"links": [
-		{ "label": "Repository", "url": pkg.repository },
-		{ "label": "Issues", "url": pkg.repository + "/issues" }
+	// topbar
+	links: [
+		{"label": "Repository", "url": pkg.repository},
+		{"label": "Issues", "url": pkg.repository + "/issues"}
 	],
 	// creates an outline using all the top level directories in the src folder
 	outline: [
