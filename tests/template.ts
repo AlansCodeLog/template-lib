9da167d8a83d07d531583f8de49fca22b170c26f
Alan <alanscodelog@gmail.com>
2021-05-10 18:38:49 -0300

    style: fixed tests template

--- tests/template.ts
+++ tests/template.ts
@@ -1,8 +1,8 @@
-import { test_name } from "@utils/testing"
+import { testName } from "@alanscodelog/utils"
+
 import { expect } from "@tests/chai"
 
-
-describe(test_name(), () => {
+describe(testName(), () => {
 	it("missing tests", () => {
 		expect(true).to.equal(false)
 	})
