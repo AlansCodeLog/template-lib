5140b777a42d9c1096d04213270cf3d58cbcf429
Alan North <alanscodelog@gmail.com>
2023-07-19 11:12:19 -0300

    refactor: moved to vite-plugin-dts for handling the type generation

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,26 +1,24 @@
-import { run } from "@alanscodelog/utils/node"
+import { escapeRegex } from "@alanscodelog/utils"
 import glob from "fast-glob"
 import { builtinModules } from "module"
 import path from "path"
-import type { PluginOption } from "vite"
+import dts from "vite-plugin-dts"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 import packageJson from "./package.json"
 
 
-const typesPlugin = (): PluginOption => ({
-	name: "typesPlugin",
-	// eslint-disable-next-line no-console
-	writeBundle: async () => run("npm run build:types").promise.catch(e => console.log(e)).then(() => undefined),
-})
 
 // https://vitejs.dev/config/
 export default async ({ mode }: { mode: string }) => defineConfig({
 	plugins: [
 		// even if we don't use aliases, this is needed to get imports based on baseUrl working
 		tsconfigPaths(),
-		typesPlugin(),
+		dts({
+			entryRoot: "src",
+			tsconfigPath: "tsconfig.types.json",
+		}),
 	],
 	build: {
 		outDir: "dist",
