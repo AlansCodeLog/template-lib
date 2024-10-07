0c85acc17b19ea4478019eed168685804c9b7840
Alan North <alanscodelog@gmail.com>
2024-10-07 19:23:21 -0300

    feat: new eslint config

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -3,19 +3,20 @@ export default tsEslintConfig( // this is just a re-export of tsEslint.config
 	// https://github.com/AlansCodeLog/eslint-config
 	...vueConfig,
 	{
-		files: [`**/*.{${allFileTypes.join(",")}}`],
-		// if experimentalUseProjectService is getting overloaded
-		// languageOptions: {
-		// 	parserOptions: {
-		// 		// eslint-disable-next-line camelcase
-		// 		EXPERIMENTAL_useProjectService: false,
-		// 		project: "./tsconfig.eslint.json",
-		// 	},
-		// },
 		ignores: [
 			// ...
 		],
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					// allowDefaultProject: [...allowDefaultProjectGlobs, "*.vue"]
+				},
+			},
+		},
 	},
+	{
+		// files: [`**/*.{${allFileTypes.join(",")}}`],
+	}
 	// RULE LINKS
 	// Eslint: https://eslint.org/docs/rules/
 	// Typescript: https://typescript-eslint.io/rules/
